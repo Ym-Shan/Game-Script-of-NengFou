1d88adc93c8d9f36bad7de3919c4b24f584f495e
Yimeng Shan <121172737+Ym-Shan@users.noreply.github.com>
2023-12-14 21:17:06 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,72 +2,71 @@ import pyautogui
 from PIL import Image
 import time
 import keyboard
-from PIL import Image
 import imagehash
 import time
 import sys
 
 def take_screenshot_before(x, y, width, height):
-    # 截取指定区域的屏幕
+    # Capture the specified region of the screen
     screenshot = pyautogui.screenshot(region=(x, y, width, height))
 
-    # 保存截屏文件
+    # Save the screenshot file
     screenshot.save("demo_before_picture.png")
 
-    # 返回截屏图像对象
+    # Return the screenshot image object
     return screenshot
 
 def take_screenshot(x, y, width, height, name):
-    # 截取指定区域的屏幕
+    # Capture the specified region of the screen
     screenshot = pyautogui.screenshot(region=(x, y, width, height))
 
-    # 保存截屏文件
+    # Save the screenshot file
     screenshot.save(name)
 
-    # 返回截屏图像对象
+    # Return the screenshot image object
     return screenshot
 
 def before():
-    # 在代码中指定截屏区域
-    x = 113  # 请替换为你希望的起始横坐标
-    y = 403  # 请替换为你希望的起始纵坐标
-    width = 394  # 请替换为你希望的宽度
-    height = 251  # 请替换为你希望的高度
+    # Specify the screenshot region in the code
+    x = 113  # Replace with the desired starting x-coordinate
+    y = 403  # Replace with the desired starting y-coordinate
+    width = 394  # Replace with the desired width
+    height = 251  # Replace with the desired height
     take_screenshot_before(x, y, width, height)
     return determine_before_Truebefore("demo_before_picture.png")
 
 def new1():
-    x = 22  # 请替换为你希望的起始横坐标
-    y = 906  # 请替换为你希望的起始纵坐标
-    width = 163  # 请替换为你希望的宽度
-    height = 121  # 请替换为你希望的高度
+    x = 22  # Replace with the desired starting x-coordinate
+    y = 906  # Replace with the desired starting y-coordinate
+    width = 163  # Replace with the desired width
+    height = 121  # Replace with the desired height
     take_screenshot(x, y, width, height, 'pic1.png')
     return determine("pic1.png")
 
 def new2():
-    x = 228  # 请替换为你希望的起始横坐标
-    y = 906  # 请替换为你希望的起始纵坐标
-    width = 163  # 请替换为你希望的宽度
-    height = 121  # 请替换为你希望的高度
+    x = 228  # Replace with the desired starting x-coordinate
+    y = 906  # Replace with the desired starting y-coordinate
+    width = 163  # Replace with the desired width
+    height = 121  # Replace with the desired height
     take_screenshot(x, y, width, height, 'pic2.png')
     return determine("pic2.png")
+
 def new3():
-    x = 434  # 请替换为你希望的起始横坐标
-    y = 906  # 请替换为你希望的起始纵坐标
-    width = 163  # 请替换为你希望的宽度
-    height = 121  # 请替换为你希望的高度
+    x = 434  # Replace with the desired starting x-coordinate
+    y = 906  # Replace with the desired starting y-coordinate
+    width = 163  # Replace with the desired width
+    height = 121  # Replace with the desired height
     take_screenshot(x, y, width, height, 'pic3.png')
     return determine("pic3.png")
 
 def image_similarity(image_path1, image_path2):
-    # 使用平均哈希算法计算图像的哈希值
+    # Calculate the hash value of the images using the average hash algorithm
     hash1 = imagehash.average_hash(Image.open(image_path1))
     hash2 = imagehash.average_hash(Image.open(image_path2))
 
-    # 比较两个图像的哈希值，返回相似性分数
+    # Compare the hash values of two images and return a similarity score
     similarity_score = hash1 - hash2
     return similarity_score
-
 
 def choose_matching_image(existing_image_path, image_paths):
     existing_image_hash = imagehash.average_hash(Image.open(existing_image_path))
@@ -75,15 +74,15 @@ def choose_matching_image(existing_image_path, image_paths):
     for image_path in image_paths:
         similarity_score = image_similarity(existing_image_path, image_path)
 
-        # 设置一个阈值，表示相似性的限定
+        # Set a threshold to determine similarity
         threshold = 5
 
         if similarity_score < threshold:
-            print(f"找到相同的图片：{image_path}")
+            print(f"Found a matching image: {image_path}")
             return image_path
 
-    print("未找到相同的图片")
-    raise SystemExit              # 停止程序
+    print("No matching image found")
+    raise SystemExit  # Stop the program
     return None
 
 def determine_before_Truebefore(path):
@@ -159,19 +158,19 @@ def click(flag_big, flag_1, flag_2, flag_3):
         pyautogui.click(500, 950)
 
 if __name__ == "__main__":
-    flag_1 = 11               # 小图真实编号
+    flag_1 = 11  # Small image real number
     flag_2 = 11
     flag_3 = 11
 
-    flag_big = 11           # 当前的大图对应的图编号
-    flag_big = before()         # 获得大图真实编号
+    flag_big = 11  # Current big image corresponding image number
+    flag_big = before()  # Get the real number of the big image
     pyautogui.click(300, 1100)
     time.sleep(1)
     while True:
         flag_1 = new1()
         flag_2 = new2()
         flag_3 = new3()
-        flag_big_next = before()  # 获得大图真实编号
+        flag_big_next = before()  # Get the real number of the big image
         click(flag_big, flag_1, flag_2, flag_3)
         flag_big = flag_big_next
         time.sleep(0.2)
